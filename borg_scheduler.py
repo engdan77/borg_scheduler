c26245acb88e263026b602ccd872f2231d365e83
chrome <daniel@engvalls.eu>
2018-08-01 10:26:28 +0200

    Initial

---
+++ borg_scheduler.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+
+import subprocess
+import time
+
+BORG_PASSPHRASE = "xxx"
+date = str(time.strftime("%Y.%m.%d_%H:%M:%S"))
+ssh_port = 22
+borg_ssh_port = 9923
+ssh_username = "edo"
+borg_username = "edo"
+base_repository = "/home/edo/tmp/borg/"
+
+backup_include_default = []
+backup_exclude_default = []
+
+'''
+backup_server_all = [
+  ("server1",  "1.1.1.1",  BORG_PASSPHRASE, [ "/var/www", "/var/vmail", "/data/backup_sql"], []),
+  ("server2",  "2.2.2.2",  BORG_PASSPHRASE, [ "/var/www", "/var/vmail", "/data/backup_sql"], []),
+  ("server3",  "3.3.3.3",  BORG_PASSPHRASE, [ "/var/www", "/var/vmail"], []),
+  ("server3",  "4.4.4.4",  BORG_PASSPHRASE, [], []),
+]
+'''
+backup_server_all = [
+    ("server1", "127.0.0.1", BORG_PASSPHRASE, ["/home/edo/tmp/backup_dir"], []),
+]
+
+
+def shellCmd(cmd):
+    print "\nCMD: \n" + cmd
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (output, err) = proc.communicate()
+    if output is not None:
+        if len(output) > 0:
+            print output
+    if err is not None:
+        if len(err) > 0:
+            print err
+    return output
+
+
+def connectSsh(host_address, ssh_port, borg_ssh_port, ssh_username, cmd):
+    ssh_cmd = '/usr/bin/ssh %s@%s -p %s -R %s:localhost:%s "%s"' % (ssh_username, host_address, ssh_port, borg_ssh_port, ssh_port, cmd)
+    shellCmd(ssh_cmd)
+
+
+def backup(host_name, host_address, borg_passphrase, backup_include, backup_exclude):
+    repository = "ssh://%s@localhost:%s/%s" % (borg_username, borg_ssh_port, base_repository + host_name)
+    borg_pass = "export BORG_PASSPHRASE='%s' && " % borg_passphrase
+    borg_create = "borg create --compression lz4 -v --stats %s::%s-%s %s %s" % (repository, host_name, date, " ".join(backup_include_default + backup_include), " --exclude ".join(backup_exclude_default + backup_exclude))
+    borg_prune = "borg prune -v %s --prefix %s --keep-daily=7 --keep-weekly=4 --keep-monthly=6" % (repository, host_name)
+
+    connectSsh(host_address, ssh_port, borg_ssh_port, ssh_username, borg_pass + borg_create)
+    connectSsh(host_address, ssh_port, borg_ssh_port, ssh_username, borg_pass + borg_prune)
+
+for host_name, host_address, borg_passphrase, backup_include, backup_exclude in backup_server_all:
+    backup(host_name, host_address, borg_passphrase, backup_include, backup_exclude)
